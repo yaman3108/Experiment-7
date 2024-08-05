383d71e8bbd2ef5f672ac11f067dd237e5792acb
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-08-05 15:28:37 +0530

    Create EXP7.1.cpp

---
+++ EXP7.1.cpp
@@ -0,0 +1,15 @@
+#include<iostrean>
+using namespace std;
+
+int main()
+{
+  int array[5]={19,10,8,17,9};
+  int array2[]={19,10,8,17,9};
+  int array3[5]={19,10,8};
+
+cout<<"n Traditional Method";
+for(int i=0;i<5;++i){
+ cout<<array1[i]<<" ";
+}
+cout<<endl;
+}
