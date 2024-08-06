3e6e61643b7e92e0120d8f07ba3172f73493a987
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-08-06 15:36:51 +0530

    Update EXP7.2.cpp

--- EXP7.2.cpp
+++ EXP7.2.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
+#define size5
 using namespace std;
 
-
+//Name:Yaman Hasan Ansari PRN:230701231552
 // Sum of elements 
       for(int k = 0; k<size5; k++){
             sum5 = sum5 + arr3[k];
