d68f4c4fee6d571a614a696b1455ff07dd92be4a
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-08-06 15:06:00 +0530

    Create EXP7.2.cpp

---
+++ EXP7.2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+
+// Sum of elements 
+      for(int k = 0; k<size5; k++){
+            sum5 = sum5 + arr3[k];
+      }
+      cout<<"The sum of the elements in the array is:"<<sum5;
