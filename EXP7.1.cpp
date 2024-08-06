be0bb27c27a8b0db70bcf05b5cfb6bbed7c6c12a
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-08-06 15:01:41 +0530

    Update EXP7.1.cpp

--- EXP7.1.cpp
+++ EXP7.1.cpp
@@ -1,12 +1,15 @@
 #include<iostrean>
 using namespace std;
 
+//Name: Yaman Hasan Ansari
 int main()
 {
   int array[5]={19,10,8,17,9};
   int array2[]={19,10,8,17,9};
   int array3[5]={19,10,8};
 
+  //Printing elements in an array
+  
 cout<<"n Traditional Method";
 for(int i=0;i<5;++i){
  cout<<array1[i]<<" ";
