2d80d5f3d1e88e122e6899aee5c6d435d3361e03
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-08-07 14:20:31 +0530

    Create EXP7.6.cpp

---
+++ EXP7.6.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+//Name: Yaman Hasan Ansari PRN:23070123155
+//Max and min
+int main() {
+int n, i, max=0;
+cout<<"Enter number of elements: ";
+cin>>n;
+int a[n];
+for(i=0;i<n;i++){
+cout<<"Enter element-"<<i<<": ";
+cin>>a[i];
+}
+for(i=0;i<n;i++){
+if(a[i]>max){
+max=a[i];
+}
+}
+int min=a[0];
+for(i=0;i<n;i++){
+if(min>a[i]){
+min=a[i];
+}
+}
+cout<<"Maximum: "<<max<<endl<<"Minimum: "<<min;
+}
