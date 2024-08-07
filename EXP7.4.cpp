6d985e280e72dd170db41a636405f5cea84a5c6e
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-08-07 14:09:30 +0530

    Create EXP7.4.cpp

---
+++ EXP7.4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+//Name: Yaman Hasan Ansari PRN:23070123155
+int main() {
+int marks[5], i, j, num, flag=0, count=0;
+for(i=0;i<5;i++) {
+cout<<"Enter Marks-"<<i+1<<": ";
+cin>>marks[i];
+}
+cout<<"Enter marks to be searched: ";
+cin>>num;
+for(j=0;j<5;j++) {
+if(marks[j]==num) {
+cout<<"Position of "<<num<<": "<<j+1<<endl;
+count++;
+flag=1;
+}
+}
+if(flag==0) {
+cout<<"marks not present";
+}
+else if(flag==1) {
+cout<<"marks are present: "<<count<<" times";
+}
+}
