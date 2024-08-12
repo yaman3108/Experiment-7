6d40cd7e9d68339cd64a743a855f4666de4991d1
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-08-12 14:58:19 +0530

    Create EXP7.7.cpp

---
+++ EXP7.7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//Name:Yaman Hasan Ansari PRN:23070123155
+int main() {
+string a;
+cout<<"Enter a string: ";
+cin>>a;
+cout<<"Entered string is "<<a<<endl;
+return 0;
+}
