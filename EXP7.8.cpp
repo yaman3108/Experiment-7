8473a0ed1029dd262e3e6c798e2f52207e9612ac
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-08-12 15:05:45 +0530

    Create EXP7.8.cpp

---
+++ EXP7.8.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//Name:Yaman Hasan Ansari PRN:23070123155
+int main() {
+string a, b;
+cout<<"Enter two strings: ";
+cin>>a>>b;
+cout<<"Concatenated string is "<<a+b;
+return 0;
+}
